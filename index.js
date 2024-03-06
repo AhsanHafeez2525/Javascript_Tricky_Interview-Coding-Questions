17d68c3dc2e3c415c246291e3d2bddf9aa7f305c
Ahsan <ahsansatti402@gmail.com>
2024-03-06 23:48:57 +0500

    promise

--- index.js
+++ index.js
@@ -487,3 +487,30 @@ function outer() {
 }
 outer();
 console.log('TOO OUTER', username);
+
+// Promise
+
+function myDisplayer(some) {
+	document.getElementById('demo').innerHTML = some;
+}
+
+let myPromise = new Promise(function (myResolve, myReject) {
+	let x = 0;
+
+	// The producing code (this may take some time)
+
+	if (x == 0) {
+		myResolve('OK');
+	} else {
+		myReject('Error');
+	}
+});
+
+myPromise.then(
+	function (value) {
+		myDisplayer(value);
+	},
+	function (error) {
+		myDisplayer(error);
+	}
+);
