be9668877e8ea4b6bdeb1135ec7eeff2e0bac16b
Ahsan <ahsansatti402@gmail.com>
2024-03-06 23:50:11 +0500

    callback

--- index.js
+++ index.js
@@ -514,3 +514,20 @@ myPromise.then(
 		myDisplayer(error);
 	}
 );
+
+// callback
+
+function getFile(myCallback) {
+	let req = new XMLHttpRequest();
+	req.open('GET', 'mycar.html');
+	req.onload = function () {
+		if (req.status == 200) {
+			myCallback(req.responseText);
+		} else {
+			myCallback('Error: ' + req.status);
+		}
+	};
+	req.send();
+}
+
+getFile(myDisplayer);
