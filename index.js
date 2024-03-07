49b92982e4c97493d73b7c19c185a81ec6c52800
Ahsan <ahsansatti402@gmail.com>
2024-03-07 23:56:03 +0500

    asyncawait-agian

--- index.js
+++ index.js
@@ -551,3 +551,23 @@ async function getFile() {
 }
 
 getFile();
+
+// async await
+
+async function getFile() {
+	let myPromise = new Promise(function (resolve) {
+		let req = new XMLHttpRequest();
+		req.open('GET', 'mycar.html');
+		req.onload = function () {
+			if (req.status == 200) {
+				resolve(req.response);
+			} else {
+				resolve('File not Found');
+			}
+		};
+		req.send();
+	});
+	document.getElementById('demo').innerHTML = await myPromise;
+}
+
+getFile();
